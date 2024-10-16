ac33096cc16c1426964227fbdb2083c40c608af3
GunTsy <carlosalexandre3254@gmail.com>
2024-10-16 11:01:34 -0300

    initial cummit

---
+++ scripts.js
@@ -0,0 +1,38 @@
+// Cotação de moedas do dia.
+const USD = 4.87
+const EUR = 5.32
+const GBP = 6.08
+
+// Obtendo os elementos
+const form = document.querySelector("form")
+const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
+
+// Manipulando o input amount para receber apenas numeros
+amount.addEventListener("input", () => {
+  const hasCharactersRegex = /\D+/g
+  amount.value = amount.value.replace(hasCharactersRegex, "")
+})
+
+// Capturando o evento de submite do formulario
+form.onsubmit = (event) => {
+  event.preventDefault()
+
+  switch (currency.value){
+    case "USD": 
+      convertCurrency(amount.value, USD, "US$")
+      break
+    case "EUR":
+      convertCurrency(amount.value, EUR, "€")
+      break
+    case "GBP":
+      convertCurrency(amount.value, GBP, "£")
+      break
+  }
+}
+
+// Função para converter a moeda
+
+function convertCurrency(amount, price, symbol) {
+
+}
